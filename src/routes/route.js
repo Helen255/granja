3c84a1f1a02662e1b04787d3ed34972fe396d1bc
Helen <helen@trigueros.com>
2020-09-23 03:45:35 -0600

    Ruta creada para poder insertar datos en la tabala categoria, por medio de procedimiento almacenado

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,36 @@
 const express = require('express');
+const mysqlConnection = require('../database');
 const router = express.Router();
 
+
+
+router.get('/',(req, res) => {
+    mysqlConnection.query('SELECT * FROM categoria', (err, rows, fields ) => {
+        if(!err) {
+            res.json(rows);
+        }else {
+            console.log(err);
+        }
+    });
+});
+
+
+router.post('/', (req, res) => {
+    const { id, nombre, descripcion } = req.body;
+    const query = `
+    SET @id = ?;
+    SET @nombre = ?;
+    SET @descripcion = ?;
+    CALL categorias(@id, @nombre, @descripcion);
+    `;
+    mysqlConnection.query(query, [id, nombre, descripcion], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Categoria guardada'});
+        }else {
+            con.log(err);
+        }
+
+
+    });
+});
 module.exports = router;
