16b10dc7260b3853b24a96c3f142df386cbb969e
Helen <helen@trigueros.com>
2020-09-23 04:16:10 -0600

    Ruta creada para actualizar datos de la tabla categoria

--- src/routes/route.js
+++ src/routes/route.js
@@ -17,20 +17,32 @@ router.get('/',(req, res) => {
 
 router.post('/', (req, res) => {
     const { id, nombre, descripcion } = req.body;
-    const query = `
-    SET @id = ?;
-    SET @nombre = ?;
-    SET @descripcion = ?;
-    CALL categorias(@id, @nombre, @descripcion);
+    const query = ` 
+    CALL categorias(?, ?, ?);
     `;
     mysqlConnection.query(query, [id, nombre, descripcion], (err, rows, fields) => {
         if(!err) {
             res.json({Status: 'Categoria guardada'});
         }else {
-            con.log(err);
+            console.log(err);
         }
 
 
     });
 });
+
+router.put('/:id', (req, res) => {
+   const {nombre, descripcion} = req.body;
+   const { id } = req.params;
+   const query= 'CALL categorias(?, ?, ?)';
+   mysqlConnection.query(query, [id, nombre, descripcion], (err, rows, fields) => {
+       if(!err) {
+           res.json({Status: 'Categoria actualizada'});
+       }else {
+           console.log(err);
+       }
+   });
+});
+
+
 module.exports = router;
