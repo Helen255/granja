606a7c37bbdced2ff5d5837f3e3e8325e8e130fe
Helen <helen@trigueros.com>
2020-09-23 22:59:32 -0600

    Creación de la dirección de la ruta usuario.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(require('./routes/categoria'));
 app.use(require('./routes/consumo'));
 app.use(require('./routes/compra'));
+app.use(require('./user/compra'));
 
 //Inicializando servidor
 app.listen(app.get('port'), () => {
