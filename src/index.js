8a489fb523e4d75edb15b8f7d172a8e8638e4ef3
Helen <helen@trigueros.com>
2020-09-24 21:40:37 -0600

    Creación de la dirección de la ruta ventas.js

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ app.use(require('./app/clientes'));
 app.use(require('./app/proveedores'));
 app.use(require('./app/telefonos'));
 app.use(require('./app/direcciones'));
+app.use(require('./app/ventas'));
 
 //Inicializando servidor
 app.listen(app.get('port'), () => {
