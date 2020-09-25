31870e2da071418207e9c3225eccf5ad46b8769d
Helen <helen@trigueros.com>
2020-09-24 23:23:46 -0600

    Creación de la petición PUT de la tabla detalleVentas

--- src/app/detalle.js
+++ src/app/detalle.js
@@ -14,4 +14,35 @@ router.get('/detalle',(req, res) => {
     });
 });
 
+//peticiones post
+router.post('/detalle', (req, res) => {
+    const { id, precio, cantidad, total, subtotal, venta_id, producto_id } = req.body;
+    const query = ` 
+    CALL detalleVentas(?, ?, ?, ?, ?, ?, ?);
+    `;
+    mysqlConnection.query(query, [id, precio, cantidad, total, subtotal, venta_id, producto_id ], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Detalle venta guardado'});
+        }else {
+            console.log(err);
+        }
+
+
+    });
+});
+
+//Peticiones put
+router.put('/detalle/:id', (req, res) => {
+    const {precio, cantidad, total, subtotal, venta_id, producto_id } = req.body;
+    const { id } = req.params;
+    const query= 'CALL detalleVentas(?, ?, ?, ?, ?, ?, ?)';
+    mysqlConnection.query(query, [id, precio, cantidad, total, subtotal, venta_id, producto_id ], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Detalle  actualizado'});
+        }else {
+            console.log(err);
+        }
+    });
+ });
+
 module.exports = router;
