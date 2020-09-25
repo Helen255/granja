49d0375766b3fa26c3f92f6b9fb9d98b115cc6fd
Helen <helen@trigueros.com>
2020-09-24 21:52:57 -0600

    Creación de la petición PUT de la tabla venta

--- src/app/ventas.js
+++ src/app/ventas.js
@@ -31,4 +31,18 @@ router.post('/ventas', (req, res) => {
     });
 });
 
+//Peticiones put
+router.put('/ventas/:id', (req, res) => {
+    const {total, fecha_venta, usuario_id} = req.body;
+    const { id } = req.params;
+    const query= 'CALL ventas(?, ?, ?)';
+    mysqlConnection.query(query, [id, total, fecha_venta, usuario_id], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Venta  actualizada'});
+        }else {
+            console.log(err);
+        }
+    });
+ });
+ 
 module.exports = router;
