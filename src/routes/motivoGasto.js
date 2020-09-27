4c05dd14b2185dcae785edf8fc5da66120e15e2a
Helen <helen@trigueros.com>
2020-09-27 05:36:12 -0600

    Creación de la petición POST de la tabla motivoGasto

--- src/routes/motivoGasto.js
+++ src/routes/motivoGasto.js
@@ -14,4 +14,35 @@ router.get('/motivoGasto',(req, res) => {
     });
 });
 
+//peticiones post
+router.post('/motivoGasto', (req, res) => {
+    const { id, tipo_gasto } = req.body;
+    const query = ` 
+    CALL motivoGastos(?, ?);
+    `;
+    mysqlConnection.query(query, [id, tipo_gasto], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Tipo gasto guardado'});
+        }else {
+            console.log(err);
+        }
+
+
+    });
+});
+
+//Peticiones de actualización
+router.put('/motivoGasto/:id', (req, res) => {
+    const { tipo_gasto} = req.body;
+    const { id } = req.params;
+    const query= 'CALL motivoGastos(?, ?)';
+    mysqlConnection.query(query, [id, tipo_gasto], (err, rows, fields) => {
+        if(!err) {
+            res.json({Status: 'Motivo gasto actualizado'});
+        }else {
+            console.log(err);
+        }
+    });
+ });
+
 module.exports = router;
