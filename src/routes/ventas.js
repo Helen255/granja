46f2cc6e6b6b655864a8977708816cd7562487a6
Helen <helen@trigueros.com>
2020-09-27 00:11:22 -0600

    Modifique nombre de atributos de la tabla ventas

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -16,11 +16,11 @@ router.get('/ventas',(req, res) => {
 
 //peticiones post
 router.post('/ventas', (req, res) => {
-    const { id, total, fecha_venta, usuario_id} = req.body;
+    const { id, total, fecha_venta, usuario_id, cliente_id}, cliente_id = req.body;
     const query = ` 
-    CALL ventas(?, ?, ?, ?);
+    CALL ventas(?, ?, ?, ?, ?);
     `;
-    mysqlConnection.query(query, [id,  total, fecha_venta, usuario_id], (err, rows, fields) => {
+    mysqlConnection.query(query, [id,  total, fecha_venta, usuario_id, cliente_id], (err, rows, fields) => {
         if(!err) {
             res.json({Status: 'Venta guardada'});
         }else {
@@ -33,10 +33,10 @@ router.post('/ventas', (req, res) => {
 
 //Peticiones put
 router.put('/ventas/:id', (req, res) => {
-    const {total, fecha_venta, usuario_id} = req.body;
+    const {total, fecha_venta, usuario_id, cliente_id} = req.body;
     const { id } = req.params;
-    const query= 'CALL ventas(?, ?, ?, ?)';
-    mysqlConnection.query(query, [id, total, fecha_venta, usuario_id], (err, rows, fields) => {
+    const query= 'CALL ventas(?, ?, ?, ?, ?)';
+    mysqlConnection.query(query, [id, total, fecha_venta, usuario_id, cliente_id], (err, rows, fields) => {
         if(!err) {
             res.json({Status: 'Venta  actualizada'});
         }else {
