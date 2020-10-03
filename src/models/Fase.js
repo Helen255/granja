8b40be68236aca1f19a72c678e279a3f779185d7
Helen <helen@trigueros.com>
2020-10-03 13:56:01 -0600

    Creación del modelo para las peticiones del crud fase

--- src/models/Fase.js
+++ src/models/Fase.js
@@ -23,4 +23,48 @@ Fase.crear = (nuevaFase, result) => {
     });
 }
 
+//Petición Get
+Fase.getList = result => {
+    conexion.query("SELECT * FROM fases", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("fase:", res);
+        result(null, res);
+    });
+};
+
+//Petición Put
+Fase.updateId = (id, faseActu, result) => {
+    const query = ` 
+    CALL fases(?, ?);
+    `;
+    conexion.query(query, [faseActu.numero_fase, id], (err, rows, fields) => {
+        if (!err) {
+            result(null, { id: result.updateId, ...faseActu })
+            return;
+        } else {
+            console.log(err);
+            result(err, null)
+            return;
+        }
+
+    });
+}
+
+//Petición Delete
+Fase.removeId = (id, result) => {
+    conexion.query("DELETE FROM fases WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("Fase id eliminada: ", id);
+        result(null, res);
+    });
+};
+
 module.exports = Fase;
