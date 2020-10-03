b7d45f9b1ed92d1d1ff8076aacaebf6fd9a1c0f4
Helen <helen@trigueros.com>
2020-10-03 13:55:40 -0600

    Creación del modelo para las peticiones del crud empleado

--- src/models/Empleado.js
+++ src/models/Empleado.js
@@ -3,9 +3,9 @@ const conexion = require('../db');
 
 const Empleado = function (empleado) {
     this.id = empleado.id;
-    this.nombre = empleado.nombre;
     this.dpi = empleado.dpi;
     this.puesto = empleado.puesto;
+    this.nombre = empleado.nombre; 
     this.usuario_id = empleado.usuario_id
 }
 
@@ -13,7 +13,7 @@ Empleado.crear = (nuevoEmpleado, result) => {
     const query = ` 
     CALL empleados(?, ?, ?, ?, ?);
     `;
-    conexion.query(query, [nuevoEmpleado.id, nuevoEmpleado.nombre, nuevoEmpleado.dpi, nuevoEmpleado.puesto, nuevoEmpleado.usuario_id], (err, rows, fields) => {
+    conexion.query(query, [nuevoEmpleado.id, nuevoEmpleado.dpi, nuevoEmpleado.puesto, nuevoEmpleado.nombre, nuevoEmpleado.usuario_id], (err, rows, fields) => {
         if (!err) {
             result(null, { id: result.insertId, ...nuevoEmpleado })
             return;
@@ -25,5 +25,50 @@ Empleado.crear = (nuevoEmpleado, result) => {
 
     });
 }
+
+//Petición Get
+Empleado.getList = result => {
+    conexion.query("SELECT * FROM empleado", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("empleado:", res);
+        result(null, res);
+    });
+};
+
+//Petición Put
+Empleado.updateId = (id, empleadoActu, result) => {
+    const query = ` 
+    CALL empleados(?, ?, ?, ?, ?);
+    `;
+    conexion.query(query, [empleadoActu.nombre, empleadoActu.dpi, empleadoActu.puesto, empleadoActu.usuario_id, id], (err, rows, fields) => {
+        if (!err) {
+            result(null, { id: result.updateId, ...empleadoActu })
+            return;
+        } else {
+            console.log(err);
+            result(err, null)
+            return;
+        }
+
+    });
+}
+
+//Petición Delete
+Empleado.removeId = (id, result) => {
+    conexion.query("DELETE FROM empleado WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("Empleado id eliminado: ", id);
+        result(null, res);
+    });
+};
+
 
 module.exports = Empleado;
