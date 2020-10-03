03e34ef9fa3c7d44964a1185b107dd6d60bf2125
Helen <helen@trigueros.com>
2020-10-02 22:40:49 -0600

    Creación de las rutas para las peticiones del crud cliente

--- src/routes/cliente_ruta.js
+++ src/routes/cliente_ruta.js
@@ -2,15 +2,15 @@ const express = require('express');
 var router = express.Router();
 const clienteControlador=require('../controllers/cliente_controlador');
 
-router
-.route('/')
-//.get(categoriaControlador.lista)
-.post(clienteControlador.create);
+router.route('/')
+.get(clienteControlador.list)
+.post(clienteControlador.create)
 
- /*router.route('/:songId')
-    .get(songController.show)
-    .patch(songController.update)
-    .delete(songController.destroy);*/
+router.route('/:clienteId')
+.put(clienteControlador.update)
+
+router.route('/:clienteId')
+.delete(clienteControlador.delete)
 
 
 module.exports = router;
