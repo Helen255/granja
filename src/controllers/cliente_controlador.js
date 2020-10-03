fa39c7148b480f163a23aabc2e696bdcafc5fe7f
Helen <helen@trigueros.com>
2020-10-02 22:37:20 -0600

    Creación del crud en el controlador cliente

--- src/controllers/cliente_controlador.js
+++ src/controllers/cliente_controlador.js
@@ -14,3 +14,35 @@ exports.create = (req, res) => {
         else res.send(data);
     });
 }
+
+//Petición Get
+exports.list = (req, res) => {
+    Cliente.getList((err, data) => {
+        if (err)
+            console.log(err);
+        else res.send(data);
+    });
+};
+
+
+exports.update = (req, res) => {
+    Cliente.updateId(
+        req.params.clienteId,
+        new Cliente(req.body),
+        (err, data) => {
+            if (err)
+                console.log(err);
+            else res.send(data);
+
+        }
+    );
+
+};
+
+exports.delete = (req, res) => {
+    Cliente.removeId(req.params.clienteId, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else res.send({ message: `Cliente eliminado!` });
+    });
+};
