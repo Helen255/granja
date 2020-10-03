2db1d99152e4ac432a459ee06292d77accee8435
Helen <helen@trigueros.com>
2020-10-03 13:56:26 -0600

    Creación del modelo para las peticiones del crud direccion

--- src/models/direccion.js
+++ src/models/direccion.js
@@ -1,11 +1,11 @@
 const { query } = require('express');
 const conexion = require('../db');
 
-const Direccion = function (compra) {
-    this.id = compra.id;
-    this.direccion = compra.direccion;
-    this.cliente_id = compra.cliente_id;
-    this.empleado_id = compra.empleado_id;
+const Direccion = function (direccion) {
+    this.id = direccion.id;
+    this.direccion = direccion.direccion;
+    this.cliente_id = direccion.cliente_id;
+    this.empleado_id = direccion.empleado_id;
 }
 
 Direccion.crear = (nuevaDireccion, result) => {
@@ -24,5 +24,50 @@ Direccion.crear = (nuevaDireccion, result) => {
 
     });
 }
+
+//Petición Get
+Direccion.getList = result => {
+    conexion.query("SELECT * FROM direccion", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("dirección:", res);
+        result(null, res);
+    });
+};
+
+//Petición Put
+Direccion.updateId = (id, direccionActu, result) => {
+    const query = ` 
+    CALL direcciones(?, ?, ?, ?);
+    `;
+    conexion.query(query, [direccionActu.direccion, direccionActu.cliente_id, direccionActu.empleado_id, id], (err, rows, fields) => {
+        if (!err) {
+            result(null, { id: result.updateId, ...direccionActu })
+            return;
+        } else {
+            console.log(err);
+            result(err, null)
+            return;
+        }
+
+    });
+}
+
+
+//Petición Delete
+Direccion.removeId = (id, result) => {
+    conexion.query("DELETE FROM direccion WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("Dirección id eliminada: ", id);
+        result(null, res);
+    });
+};
 
 module.exports = Direccion;
