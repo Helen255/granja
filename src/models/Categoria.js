5ea7b7471fb5cf9c7d6d54c598e7aee3e68a958e
Helen <helen@trigueros.com>
2020-10-02 22:00:29 -0600

    Creación del modelo para las peticiones del crud categoria

--- src/models/Categoria.js
+++ src/models/Categoria.js
@@ -1,6 +1,7 @@
 const { query } = require('express');
 const conexion = require('../db');
 
+//Metodo constructor
 const Categoria = function (categoria) {
     this.id = categoria.id;
     this.nombre = categoria.nombre;
@@ -23,5 +24,48 @@ Categoria.crear = (nuevaCategoria, result) => {
 
     });
 }
+//Petición Get
+Categoria.getList = result => {
+    conexion.query("SELECT * FROM categoria", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("categoria:", res);
+        result(null, res);
+    });
+};
+
+//Petición Put
+Categoria.updateId = (id, categoriaActu, result) => {
+    const query = ` 
+    CALL categorias(?, ?, ?);
+    `;
+    conexion.query(query, [categoriaActu.nombre, categoriaActu.descripcion, id], (err, rows, fields) => {
+        if (!err) {
+            result(null, { id: result.updateId, ...categoriaActu })
+            return;
+        } else {
+            console.log(err);
+            result(err, null)
+            return;
+        }
+
+    });
+}
+
+//Petición Delete
+Categoria.removeId = (id, result) => {
+    conexion.query("DELETE FROM categoria WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("Categoria id eliminada: ", id);
+        result(null, res);
+    });
+};
 
 module.exports = Categoria;
