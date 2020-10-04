921b5e05c8d0721d7f72262a809d04b21bacee45
Helen <helen@trigueros.com>
2020-10-04 17:43:34 -0600

    Creación de las rutas para las peticiones del crud producto

--- src/routes/producto_ruta.js
+++ src/routes/producto_ruta.js
@@ -2,15 +2,15 @@ const express = require('express');
 var router = express.Router();
 const productoControlador=require('../controllers/producto_controlador');
 
-router
-.route('/')
-//.get(categoriaControlador.lista)
-.post(productoControlador.create);
+router.route('/')
+.get(productoControlador.list)
+.post(productoControlador.create)
 
- /*router.route('/:songId')
-    .get(songController.show)
-    .patch(songController.update)
-    .delete(songController.destroy);*/
+router.route('/:productoId')
+.put(productoControlador.update)
+
+router.route('/:productoId')
+.delete(productoControlador.delete)
 
 
 module.exports = router;
