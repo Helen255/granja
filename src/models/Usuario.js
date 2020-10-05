3069986038b9e7068eeb9403ba4224a4a5ec9aa3
Helen <helen@trigueros.com>
2020-10-04 20:10:32 -0600

    Creación del modelo para las peticiones del crud usuario

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -24,4 +24,46 @@ Usuario.crear = (nuevoUsuario, result) => {
     });
 }
 
+//Petición Get
+Usuario.getList = result => {
+    conexion.query("SELECT * FROM usuario", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("usuario:", res);
+        result(null, res);
+    });
+};
+
+//Petición Put
+Usuario.updateId = (id, usuarioActu, result) => {
+    const query = "UPDATE usuario SET usuario = ?, contrasenia = ? WHERE id = ?";
+    conexion.query(query, [usuarioActu.usuario, usuarioActu.contrasenia, id], (err, rows, fields) => {
+        if (!err) {
+            result(null, { id: result.updateId, ...usuarioActu })
+            return;
+        } else {
+            console.log(err);
+            result(err, null)
+            return;
+        }
+
+    });
+}
+
+//Petición Delete
+Usuario.removeId = (id, result) => {
+    conexion.query("DELETE FROM usuario WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("Usuario id eliminado: ", id);
+        result(null, res);
+    });
+};
+
 module.exports = Usuario;
