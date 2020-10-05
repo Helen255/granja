9ce9212e952f5c3f862fc8c2785d7c97ba4b2551
Helen <helen@trigueros.com>
2020-10-04 20:20:13 -0600

    Eliminación de direciones de rutas por modificación de mvc

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 
-//Middlwares funciones ejecutables antes que las petiicones llequen al servidor
+//Middlwares funciones ejecutables antes que las peticicones llequen al servidor
 app.use(express.json());
 
 //Rutas
@@ -53,31 +53,6 @@ app.use('/telefono/', telefono);
 
 const usuario = require ('../src/routes/usuario_ruta.js');
 app.use('/usuario/', usuario);
-/*app.use(require('../src/routes/clientes'));
-app.use(require('../src/routes/gastos'));
-app.use(require('../src/routes/usuarios'));
-app.use(require('../src/routes/empleados'));
-app.use(require('../src/routes/clientes'));
-app.use(require('../src/routes/proveedores'));
-app.use(require('../src/routes/telefonos'));
-app.use(require('../src/routes/direcciones'));
-app.use(require('../src/routes/ventas'));
-app.use(require('../src/routes/productos'));
-app.use(require('../src/routes/detalle'));
-app.use(require('../src/routes/compra'));
-app.use(require('../src/routes/fase'));
-app.use(require('../src/routes/motivoGasto'));
-app.use(require('../src/routes/consumo'));
-
-//rutas vistas
-app.get('/',(req, res) => {
-    //res.sendFile(path.resolve(__dirname, '../views/index.html'));
-     res.render('index');  
-});
-
-app.get('/categorias',(req, res) => {
-     res.render('categorias');  
-});*/
 
 
 //Inicializando servidor
