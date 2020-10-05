5437e67e4f4bb42974f3e27aa9233ac41003e4e0
Helen <helen@trigueros.com>
2020-10-04 20:10:10 -0600

    Creación del crud controlador usuario

--- src/controllers/usuario_controlador.js
+++ src/controllers/usuario_controlador.js
@@ -14,3 +14,39 @@ exports.create = (req, res) => {
         else res.send(data);
     });
 }
+
+//Petición Get
+exports.list = (req, res) => {
+    Usuario.getList((err, data) => {
+        if (err)
+            console.log(err);
+        else res.send(data);
+    });
+};
+
+
+exports.update = (req, res) => {
+    Usuario.updateId(
+        req.params.usuarioId,
+        new Usuario(req.body),
+        (err, data) => {
+            if (err)
+                console.log(err);
+            else res.send(data);
+
+        }
+    );
+
+};
+
+exports.delete = (req, res) => {
+    Usuario.removeId(req.params.usuarioId, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else res.send({ message: `Usuario eliminado!` });
+    });
+};
+
+
+
+
